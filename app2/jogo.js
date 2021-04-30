8b5f87254cfe75987c21e887533d52e849f81d06
misaelorelio <misaelorelio@hotmail.com>
2021-04-29 21:27:59 -0300

    desenvolvendo a aplicação

--- app2/jogo.js
+++ app2/jogo.js
@@ -1,13 +1,33 @@
 
-var altura =0
-var largura =0
+var altura = 0
+var largura = 0
 
 function ajustaTamanhoPalcoJogo() {
 
-     altura = window.innerHeight
-     largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
 
 }
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica() {
+
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+
+}
