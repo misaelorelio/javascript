10c232b161b12f0ce8255ac35c4cdc269f6594b4
misaelorelio <misaelorelio@hotmail.com>
2021-04-30 23:01:35 -0300

    criando e removendo mosquito pelo ID

--- app2/jogo.js
+++ app2/jogo.js
@@ -13,6 +13,10 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+    
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -27,8 +31,10 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
     document.body.appendChild(mosquito)
+
 
 }
 
