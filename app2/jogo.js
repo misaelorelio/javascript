8ec0f687b2bfdd32cc2224446aefd8cd98cec4a3
misaelorelio <misaelorelio@hotmail.com>
2021-05-02 22:02:47 -0300

    nível de dificuldade do jogo

--- app2/jogo.js
+++ app2/jogo.js
@@ -4,6 +4,20 @@ var largura = 0
 var vida = 1
 var tempo = 10
 
+var criaMosquitoTempo = 1500
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal'){
+    criaMosquitoTempo = 1500
+}
+else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+}
+else if (nivel === 'ultraDificil') {
+    criaMosquitoTempo = 750
+}
+
 function ajustaTamanhoPalcoJogo() {
 
     altura = window.innerHeight
